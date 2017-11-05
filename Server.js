d2be4266a6b7007c5d00d771d923a2b439f2f62c
Tyler Kujawa <comsenzce13@gmail.com>
2017-11-05 17:28:04 -0500

    changing to serve app using http-server instead of express.js

--- Server.js
+++ Server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + 'dist')));
 // Start the app by listening on the default
 // Heroku port
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 app.listen(process.env.PORT || 8080);
+console.log('Server running on localhost:8080');
