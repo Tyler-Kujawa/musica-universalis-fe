a4843dc4bc7673accdc5e237ce228ee5e24f7440
Tyler Kujawa <comsenzce13@gmail.com>
2017-11-05 18:00:47 -0500

    removing double declaration Server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(path.join(__dirname + 'dist')));
