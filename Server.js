4f1c17c4a6a9d355e196e838ffeabdf360b36e5c
Tyler Kujawa <comsenzce13@gmail.com>
2017-11-05 17:49:51 -0500

    updating package.json to use Express server for Heroku

--- Server.js
+++ Server.js
@@ -11,9 +11,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
 app.listen(process.env.PORT || 8080);
 console.log('Server running on localhost:8080');
