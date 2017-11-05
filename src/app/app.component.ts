82656e5acdb433175db6435a25e8175018c3ef75
Tyler Kujawa <comsenzce13@gmail.com>
2017-11-05 15:04:47 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  moduleId: module.id,
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
