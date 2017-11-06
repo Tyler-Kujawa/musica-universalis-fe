0dc251181b3e2e62c7e49ff2eb137d5fe508161c
Tyler Kujawa <comsenzce13@gmail.com>
2017-11-05 19:41:35 -0500

    some more server stuff

--- Server.js
+++ Server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(path.join(__dirname + 'dist')));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
-console.log('Server running on localhost:8080');
